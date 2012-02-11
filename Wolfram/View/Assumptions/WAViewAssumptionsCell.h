245c7beda42fe5d719a3917f89a17c863e4e865f
Alex Nichol <alexnichol@comcast.net>
2012-02-11 15:08:34 -0500

    Moved pod items & added assumption setup

---
+++ Wolfram/View/Assumptions/WAViewAssumptionsCell.h
@@ -0,0 +1,13 @@
+//
+//  WAViewAssumptionsCell.h
+//  Wolfram
+//
+//  Created by Alex Nichol on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WAViewItemCell.h"
+
+@interface WAViewAssumptionsCell : WAViewItemCell
+
+@end
---
+++ Wolfram/View/Assumptions/WAViewAssumptionsCell.m
@@ -0,0 +1,13 @@
+//
+//  WAViewAssumptionsCell.m
+//  Wolfram
+//
+//  Created by Alex Nichol on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WAViewAssumptionsCell.h"
+
+@implementation WAViewAssumptionsCell
+
+@end
